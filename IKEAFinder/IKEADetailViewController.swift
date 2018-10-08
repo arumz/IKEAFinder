83d9f919db9f24e1f06d6831485658fb2a433fbb
Andrew Roembke <aroembke21@gmail.com>
2018-10-08 14:15:34 -0400

    end v.5 added labels in the detail view controller and output from selectedIKEA object. auto-layout utilized for portrait mode.

--- IKEAFinder/IKEADetailViewController.swift
+++ IKEAFinder/IKEADetailViewController.swift
@@ -11,13 +11,61 @@ import UIKit
 class IKEADetailViewController: UIViewController {
     
     @IBOutlet weak var lblStoreName: UILabel!
+    @IBOutlet weak var lblStoreNumber: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var lblCity: UILabel!
+    @IBOutlet weak var lblZipCode: UILabel!
+    @IBOutlet weak var lblCountry: UILabel!
+    @IBOutlet weak var lblTelephone: UILabel!
+    @IBOutlet weak var lblSize: UILabel!
+    @IBOutlet weak var lblNumberOfRooms: UILabel!
+    @IBOutlet weak var lblNumberOfHomes: UILabel!
+    @IBOutlet weak var lblWebpage: UILabel!
+    
+    
     
     //absolutely has be an IKEA object, cannot be nil
     var selectedIKEA: IKEA!
     
     override func viewWillAppear(_ animated: Bool) {
         print(selectedIKEA.storeName)
+        print(selectedIKEA.storeNumber)
         lblStoreName.text = selectedIKEA.storeName
+        //convert store number to string
+        let stringStoreNumber = String(selectedIKEA.storeNumber)
+        lblStoreNumber.text = stringStoreNumber
+        lblAddress.text = selectedIKEA.address
+        lblZipCode.text = selectedIKEA.zipCode
+        lblCountry.text = selectedIKEA.country
+        lblTelephone.text = selectedIKEA.telephone
+        //convert size to string
+        let stringSize = String(selectedIKEA.size)
+        lblSize.text = stringSize
+        //convert number of rooms to string
+        let stringNumberRooms = String(selectedIKEA.roomSettings)
+        lblNumberOfRooms.text = stringNumberRooms
+        //convert number of homes to string
+        let stringNumberHomes = String(selectedIKEA.realLifeHomes)
+        lblNumberOfHomes.text = stringNumberHomes
+        lblWebpage.text = selectedIKEA.webpage
+        
+       
+        
+//        storeImage: String,
+//        address: String,
+//        city: String,
+//        zipCode: String,
+//        country: String,
+//        telephone: String,
+//        size: Int,
+//        roomSettings: Int,
+//        realLifeHomes: Int,
+//        cashLanes: Int,
+//        restaurantSeating: Int,
+//        smaland: Bool,
+//        webpage: String,
+//        latitude: Double,
+//        longitude: Double
     }
     
 }
